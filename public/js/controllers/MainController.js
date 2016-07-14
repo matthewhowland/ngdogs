adaef8d867e5c59425e114d20d5e0e6f8b485e44
Matthew Howland <howlandm@gmail.com>
2016-07-14 19:48:10 -0400

    everything works, no update featured

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -5,17 +5,57 @@
   MainController.$inject = ['$scope', 'DogsService'];
 
   function MainController($scope, DogsService){
-    $scope.message = 'This is the scope message'
+    $scope.dogs = DogsService.dogs;
+    $scope.create = createDog;
+    $scope.delete = deleteDog;
+    getDogs();
 
-    var dogs;
-    DogsService.readAll()
-              .then(function(){
-                dogs = DogsService.dogs;
-                console.log(dogs);
-              });
-DogsService.create();
-DogsService.delete();
-DogsService.update();
+    function getDogs(){
+      console.log('getting dogs...');
+      DogsService.readAll()
+                .then(function(){
+                  $scope.dogs = DogsService.dogs;
+                  console.log($scope.dogs);
+                })
+    }
 
+    function createDog(dogName,
+                        breed,
+                        size,
+                        color,
+                        age,
+                        gender,
+                        timesAtPark,
+                        notes,
+                        likesDogs,
+                        likesHumans,
+                        likesKids,
+                        wantsPlaymates){
+                          console.log(color)
+      DogsService.create(dogName,
+                          breed,
+                          size,
+                          color,
+                          age,
+                          gender,
+                          timesAtPark,
+                          notes,
+                          likesDogs,
+                          likesHumans,
+                          likesKids,
+                          wantsPlaymates)
+                .then(function(){
+                  $scope.dogName = '';
+                  getDogs();
+                })
+    }
+
+    function deleteDog(id){
+      console.log(id);
+      DogsService.delete(id)
+                .then(function(){
+                  getDogs();
+                })
+    }
   }
 })();
--- public/js/services/DogsService.js
+++ public/js/services/DogsService.js
@@ -7,18 +7,42 @@
   function DogsService($http){
     var baseUrl = 'https://dogparkbuddy.herokuapp.com/'
     var o = {
-      create: createDogs,
+      create: createDog,
       readAll: getAll,
-      update: updateDogs,
-      delete: deleteDogs,
+      update: updateDog,
+      delete: deleteDog,
       dogs: []
     };
     return o;
 
-    function createDogs(newDog){
-      return $http.post(baseUrl + 'dogs/', newDog)
+    function createDog(dogName,
+                        breed,
+                        size,
+                        color,
+                        age,
+                        gender,
+                        timesAtPark,
+                        notes,
+                        likesDogs,
+                        likesHumans,
+                        likesKids,
+                        wantsPlaymates){
+      var theDog = {
+        dogName: dogName,
+        breed: breed,
+        size: size,
+        color: color,
+        age: age,
+        gender: gender,
+        timesAtPark: timesAtPark,
+        notes: notes,
+        likesDogs: likesDogs,
+        likesHumans: likesHumans,
+        likesKids: likesKids,
+        wantsPlaymates: wantsPlaymates
+      }
+      return $http.post(baseUrl + 'dogs', theDog)
               .then(function(response){
-                console.log(response);
                 getAll();
               });
     }
@@ -29,18 +53,19 @@
                     o.dogs = response.data;
                   });
     }
-    function updateDogs(id, newDog){
+    function updateDog(id, dogName){
       return $http.put(baseUrl+'dogs/'+id, newDog)
               .then(function(response){
                 console.log(response);
                 getAll();
               });
     }
-    function deleteDogs(id){}
+    function deleteDog(id){
       return $http.delete(baseUrl+'dogs/'+id)
                   .then(function(response){
-                    console.log(response);
+                    console.log('delete', response);
                     getAll();
                   })
   }
+}
 })()
